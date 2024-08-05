1b9f447a1b445d32adee0540cefc0a1afe5ac633
Jihyun247 <59338503+Jihyun247@users.noreply.github.com>
2024-08-05 23:29:21 +0900

    [Gold II] Title: 반도체 설계, Time: 28 ms, Memory: 82912 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let _ = Int(readLine()!)!
+var list = readLine()!.split(separator: " ").map{ Int(String($0))! }
+var lisList: [Int] = [list[0]]
+
+for i in 1..<list.count {
+  if lisList.last! < list[i] {
+    lisList.append(list[i])
+  } else {
+    binarySearch(&lisList, list[i])
+  }
+}
+
+func binarySearch(_ list: inout [Int], _ x: Int) {
+  var start = 0
+  var end = list.count - 1
+
+  while start < end {
+    let mid = (start + end) / 2
+    if list[mid] < x {
+      start = mid + 1
+    } else {
+      end = mid
+    }
+  }
+  list[end] = x
+}
+
+print(lisList.count)
