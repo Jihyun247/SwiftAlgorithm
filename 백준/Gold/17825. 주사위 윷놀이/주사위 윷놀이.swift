a40e48aa813d326f4af724e69042930f39d81769
Jihyun247 <59338503+Jihyun247@users.noreply.github.com>
2024-08-05 20:46:33 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 44 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.swift
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.swift
@@ -1,87 +1,105 @@
-var root = [[Int]](repeating: [Int](), count: 5)
-root[0] = (0...19).map { $0 * 2 }
-root[1] = [0, 13, 16, 19]
-root[2] = [0, 22, 24]
-root[3] = [0, 28, 27, 26]
-root[4] = [25, 30, 35, 40, 0]
+import Foundation
 
-typealias Horse = (root: Int, position: Int)
-var horses = [Horse](repeating: (0, 0), count: 4)
+typealias Meeple = (boardIdx: Int, positionIdx: Int)
 
-let dice = readLine()!.split(separator: " ").map { Int(String($0))! }
+let moves = readLine()!.components(separatedBy: " ").map { Int($0)! }
+// 탐색 공간
+// 25, 30, 35, 40이 각각 보드에 다 존재하면 안됨 (중복 체크 못함)
+// through10, **20, **30 모두 첫자리를 비워둬야함 (0으로 비워둠)
 
-var maxPoint = 0
+let straightBoard: [Int] = Array(stride(from: 0, to: 40, by: 2))
+let through10Board: [Int] = [10, 13, 16, 19]
+let through20Board:[Int] = [20, 22, 24]
+let through30Board: [Int] = [30, 28, 27, 26]
+let lastCommonBoard: [Int] = [25, 30, 35, 40, 0]
+let board: [[Int]] = [straightBoard, through10Board, through20Board, through30Board, lastCommonBoard]
 
-func move(_ horse: Horse, jump: Int) -> (Horse, Int) {
-    switch horse.root {
-        case 0:
-            let nextPosition = horse.position + jump
-            
-            if nextPosition > 20 {
-                return ((4, 4), 0)
-            } else if nextPosition == 20 {
-                return ((4, 3), 40)
-            } else if root[0][nextPosition] == 10 {
-                return ((1, 0), root[0][nextPosition])
-            } else if root[0][nextPosition] == 20 {
-                return ((2, 0), root[0][nextPosition])
-            } else if root[0][nextPosition] == 30 {
-                return ((3, 0), root[0][nextPosition])
-            } else {
-                return ((0, nextPosition), root[0][nextPosition])
-            }
-        case 1:
-            if horse.position + jump > 3 {
-                return ((4, horse.position + jump - 4), root[4][horse.position + jump - 4])
-            } else {
-                return ((1, horse.position + jump), root[1][horse.position + jump])
-            }
-        case 2:
-            if horse.position + jump > 2 {
-                return ((4, horse.position + jump - 3), root[4][horse.position + jump - 3])
-            } else {
-                return ((2, horse.position + jump), root[2][horse.position + jump])
-            }
-        case 3:
-            if horse.position + jump > 3 {
-                return ((4, horse.position + jump - 4), root[4][horse.position + jump - 4])
-            } else {
-                return ((3, horse.position + jump), root[3][horse.position + jump])
-            }
-        case 4:
-            let nextPosition = min(4, horse.position + jump)
-            return ((4, nextPosition), root[4][nextPosition])
-        default:
-            fatalError("invalid root")
+var meeples = [Meeple].init(repeating: Meeple(0,0), count: 4)
+
+var maxScore: Int = 0
+
+func moveMeeple(_ meeple: Meeple, for move: Int) -> Meeple {
+  let curBoard = board[meeple.boardIdx]
+
+  if curBoard == straightBoard { // 1. 현재 꺾는 곳 없이 가장 크게 한바퀴를 돌고 있는 경우
+    let movedIdx = meeple.positionIdx + move
+
+    if movedIdx > 20 { // movedIdx가 20을 초과한 경우, 보드판을 벗어나 lastCommonBoard의 마지막인덱스로
+      return (4, 4)
+
+    } else if movedIdx == 20 {
+      return (4, 3)
+    } else {
+      let movedScore = straightBoard[movedIdx]
+      if movedScore == 10 { // 10에서 멈춘경우, through10Board의 첫번째로 변경
+        return (1, 0)
+      } else if movedScore == 20 { // 20에서 멈춘경우, through10Board의 첫번째로 변경
+        return (2, 0)
+      } else if movedScore == 30 { // 30에서 멈춘경우, through10Board의 첫번째로 변경
+        return (3, 0)
+      } else { // 위 해당사항에 포함되지 않으면 straightBoard에서 쭉 이동
+        return (0, movedIdx)
+      }
     }
+
+  } else if curBoard == through10Board { // 2. [0, 13, 16, 19]
+    let movedIdx = meeple.positionIdx + move
+    if movedIdx > 3 { // through10Board를 벗어나면 lastCommonBoard로 이동
+      return (4, movedIdx - through10Board.count)
+    } else {
+      return (1, movedIdx)
+    }
+
+  } else if curBoard == through20Board { // 3. [0, 22, 24]
+    let movedIdx = meeple.positionIdx + move
+    if movedIdx > 2 { // through10Board를 벗어나면 lastCommonBoard로 이동
+      return (4, movedIdx - through20Board.count)
+    } else {
+      return (2, movedIdx)
+    }
+
+  } else if curBoard == through30Board { // 4. [0, 28, 27, 26]
+    let movedIdx = meeple.positionIdx + move
+    if movedIdx > 3 { // through10Board를 벗어나면 lastCommonBoard로 이동
+      return (4, movedIdx - through30Board.count)
+    } else {
+      return (3, movedIdx)
+    }
+
+  } else { // 5. curBoard == lastCommonBoard -> [25, 30, 35, 40, 0]
+    let movedIdx = min(meeple.positionIdx + move, 4)
+    return (4, movedIdx)
+  }
 }
 
-func play(_ turn: Int, _ point: Int) {
-    guard turn < 10 else {
-        if maxPoint < point {
-            maxPoint = point
-        }
-        return
+func play(_ turn: Int, _ score: Int) {
+  guard turn < 10 else {
+    if maxScore < score { maxScore = score }
+    return
+  }
+
+  for movingIdx in 0..<4 where meeples[movingIdx] != (4, 4) {
+    let meeple = meeples[movingIdx]
+
+    let movedMeeple = moveMeeple(meeple, for: moves[turn])
+    // 놓여져 있는 말과 동일한 위치에 있을 수 없으므로 비교를 한다
+      // 조건1. 이미 완주한 말은 비교대상에서 제외
+      // 조건2. 현재 이동중인 말 비교대상에서 제외
+    var canMove = true
+    for compareIdx in 0..<4 where meeples[compareIdx] != (4, 4) && compareIdx != movingIdx {
+      if meeples[compareIdx] == movedMeeple { // 이미 이동하고자 하는 위치에 다른 말이 있다면
+        canMove = false
+        break
+      }
     }
-    
-    for horseIdx in 0..<4 where horses[horseIdx] != (4, 4) {
-        let horse = horses[horseIdx]
-            
-        let (nextHorse, getPoint) = move(horses[horseIdx], jump: dice[turn])
-        var canMove = true
-        for check in 0..<4 where horses[check] != (4, 4) && check != horseIdx {
-            if horses[check] == nextHorse {
-                canMove = false
-                break
-            }
-        }
-        if canMove {
-            horses[horseIdx] = nextHorse
-            play(turn+1, point + getPoint)
-            horses[horseIdx] = horse
-        }
+
+    if canMove {
+      meeples[movingIdx] = movedMeeple
+      play(turn + 1, score + board[movedMeeple.boardIdx][movedMeeple.positionIdx])
+      meeples[movingIdx] = meeple
     }
+  }
 }
 
-play(0, 0)
-print(maxPoint)
+play(0,0)
+print(maxScore)
